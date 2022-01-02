72772915df94a928e8bdd55cc1de9787243cd69a
Allen <asd9fgh9@gmail.com>
2022-01-03 00:00:26 +0800

    'updateFV'

---
+++ js/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(e){
+        if($(window).scrollTop()>0 ) {
+            $(".navbar").removeClass("nav_top");
+        } else {
+            $(".navbar").addClass("nav_top");
+        }
+    });
+});
+
+
