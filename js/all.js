46ee5e99c70f390f57eb008d8c51e1764edef4e9
Allen <asd9fgh9@gmail.com>
2022-01-09 00:55:15 +0800

    'update-swiper'

--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
+// jQuery
 $(document).ready(function(){
     $(window).scroll(function(e){
         if($(window).scrollTop()>0 ) {
@@ -7,5 +8,48 @@ $(document).ready(function(){
         }
     });
 });
-
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
+    slidesPerView: 3,
+    spaceBetween: 40,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
 
